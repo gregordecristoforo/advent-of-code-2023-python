6fdb9ae0f0ad41b314af46698990261dd13fdaf4
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-12-02 15:54:14 +0100

    solution day 2 part 2

--- 2_day/game.py
+++ 2_day/game.py
@@ -20,6 +20,7 @@ def parse_round_string(round_str):
     return round_obj
 
 
+# part 1
 result = 0
 
 with open("data.txt") as data_file:
@@ -40,3 +41,27 @@ with open("data.txt") as data_file:
             result += i + 1
 
 print(f"Result part 1: {result}")
+
+# part 2
+result = 0
+
+with open("data.txt") as data_file:
+    for i, line in enumerate(data_file):
+        cleaned_string = line.split(":", 1)[1].strip()
+        games = [
+            parse_round_string(round_str) for round_str in cleaned_string.split(";")
+        ]
+
+        green = 0
+        red = 0
+        blue = 0
+
+        for round in games:
+            green = max(green, round.green)
+            blue = max(blue, round.blue)
+            red = max(red, round.red)
+
+        result += green * blue * red
+
+
+print(f"Result part 2: {result}")
