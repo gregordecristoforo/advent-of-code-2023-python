d049997e7f54de374303fa6dcc348931d6ba247f
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-12-04 12:40:44 +0100

    solutoin part 2

--- 4_day/cards.py
+++ 4_day/cards.py
@@ -1,16 +1,28 @@
+def get_points_per_card(card):
+    card = card.replace("\n", "")
+    _, numbers = card.split(": ")
+    winning_numbers, your_numbers = numbers.split(" | ")
+    winning_numbers = set(winning_numbers.split(" "))
+    your_numbers = set(your_numbers.split(" "))
+    winning_numbers.discard("")
+    your_numbers.discard("")
+    return len(winning_numbers.intersection(your_numbers))
+
+
 # part 1
 with open("data.txt") as data_file:
     result = 0
     for card in data_file:
-        print(card)
-        card = card.replace("\n", "")
-        _, numbers = card.split(": ")
-        winning_numbers, your_numbers = numbers.split(" | ")
-        winning_numbers = set(winning_numbers.split(" "))
-        your_numbers = set(your_numbers.split(" "))
-        winning_numbers.discard("")
-        your_numbers.discard("")
-        matching_cards = len(winning_numbers.intersection(your_numbers))
+        matching_cards = get_points_per_card(card)
         if matching_cards:
-            result += 2**(matching_cards-1)
+            result += 2 ** (matching_cards - 1)
     print(f"result part 1: {result}")
+
+# part 2
+with open("data.txt") as data_file:
+    number_of_cards = [1] * 199
+    for i, card in enumerate(data_file):
+        matching_cards = get_points_per_card(card)
+        for j in range(i + 1, i + 1 + matching_cards):
+            number_of_cards[j] += number_of_cards[i]
+    print(f"result part 2: {(sum(number_of_cards))}")
