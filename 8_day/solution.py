16be8f817b09d6314e4c84a120b1cae81cc16bcd
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-12-09 16:34:11 +0100

    day 8 solution 2 brute force

--- 8_day/solution.py
+++ 8_day/solution.py
@@ -1,13 +1,4 @@
-def circular_string_generator(input_string):
-    index = 0
-    length = len(input_string)
-    while True:
-        if length > 0:
-            yield input_string[index]
-            index = (index + 1) % length
-        else:
-            raise StopIteration
-
+from itertools import cycle
 
 lines = open("data.txt", "r").read().splitlines()
 
@@ -21,19 +12,55 @@ for line in lines[2:]:
     directions[name] = (left, right)
 
 
-circular_iterator = circular_string_generator(left_or_right)
-
 steps = 0
 position = "AAA"
-while position != "ZZZ":
-    direction = next(circular_iterator)
+# while position != "ZZZ":
+for direction in cycle(left_or_right):
+    if position == "ZZZ":
+        break
     if direction == "L":
         position = directions[position][0]
     elif direction == "R":
         position = directions[position][1]
     else:
-        raise ValueError('Directions must be wrong')
+        raise ValueError("Directions must be wrong")
 
     steps += 1
+
+print(f"Solution part 1: {steps}")
+
+
+lines = open("test_data_part_2.txt", "r").read().splitlines()
+
+left_or_right = lines[0]
+
+directions = {}
+for line in lines[2:]:
+    name, direction = line.split(" = ")
+    direction = direction.replace("(", "").replace(")", "")
+    left, right = direction.split(", ")
+    directions[name] = (left, right)
+
+
+starts = [position for position in directions if position[2] == "A"]
+steps = 0
+all_at_the_end = False
+for direction in cycle(left_or_right):
+    if all_at_the_end:
+        break
+    steps += 1
+    all_at_the_end = True
+    # print(starts)
+    for i in range(len(starts)):
+        start = starts[i]
+        if direction == "L":
+            start = directions[start][0]
+        elif direction == "R":
+            start = directions[start][1]
+        else:
+            raise ValueError("Directions must be wrong")
+        starts[i] = start
+        if start[-1] != "Z":
+            all_at_the_end = False
 
 print(steps)
